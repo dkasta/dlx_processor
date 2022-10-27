c779fb869e1807dcab53df576616117da49539fb
notogp <gianpietronoto@gmail.com>
2022-10-27 15:40:33 +0200

    Load and Store asm

--- assembly/assembler/LoadStore.asm
+++ assembly/assembler/LoadStore.asm
@@ -1,9 +1,10 @@
 myloop:
 
+addi r1, r1, #10
 addi r2, r2, #9  ;   2 <= 9                    -->  2 <= 18
-sw 20(r1), r2  	 ;   Address = 20    Data = 9  -->  Address = 20  Data = 18		
+sw 10(r1), r2  	 ;   Address = 20    Data = 9  -->  Address = 20  Data = 18		
 addi r6, r15, #0 ;   6 <= 0                    -->  6 <= 9
 addi r4, r3, #10 ;   4 <= 10                   -->  4 <= 10
 addi r7, r0, #8  ;   7 <= 8                    -->  7 <= 8
-lw r15, 20(r1)   ;   Address = 20    Data = 9  -->  Address = 20  Data = 18
+lw r15, 10(r1)   ;   Address = 20    Data = 9  -->  Address = 20  Data = 18
 jal myloop       ;   Jump and link	     
--- test/assembler.bin/LoadStore.asm
+++ test/assembler.bin/LoadStore.asm
@@ -1,9 +1,11 @@
 myloop:
 
-addi r2, r2, #9  ;   2 <= 9                    -->  2 <= 18
-sw 20(r1), r2  	 ;   Address = 20    Data = 9  -->  Address = 20  Data = 18		
-addi r6, r15, #0 ;   6 <= 0                    -->  6 <= 9
-addi r4, r3, #10 ;   4 <= 10                   -->  4 <= 10
-addi r7, r0, #8  ;   7 <= 8                    -->  7 <= 8
-lw r15, 20(r1)   ;   Address = 20    Data = 9  -->  Address = 20  Data = 18
+addi r1, r1, #10 ;   r1 <= 10
+addi r2, r2, #9  ;   r2 <= 9             
+sw 10(r1), r2  	 ;   r1 mem_forwarding_one and r2 forwarding 	
+                 ;   Address = 20   Data = 9
+addi r6, r15, #0 ;   r6 <= 0             
+addi r4, r3, #10 ;   r4 <= 10            
+addi r7, r0, #8  ;   r7 <= 8             
+lw r15, 10(r1)   ;   Address = 20   Data = 9
 jal myloop       ;   Jump and link	     
